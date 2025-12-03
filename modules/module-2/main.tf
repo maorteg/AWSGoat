632351e2d3eddf13583561d0c082b9b68f31d540
Marvin Ortega <44039920+maorteg@users.noreply.github.com>
2025-12-03 12:59:43 -0600

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -8,14 +8,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 [default]
-aws_access_key_id = AKIAX24QKKOLATS4SMHY
-aws_secret_access_key = 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
+aws_access_key_id = AKIAX24QKKOLHLJU4JGD
+aws_secret_access_key = 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
 output = json
-region = us-east-1
+region = us-east-2
 
 data "aws_caller_identity" "current" {}
 
